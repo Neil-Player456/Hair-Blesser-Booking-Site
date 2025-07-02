3d6951eadb5a8d1a94562a60a8e18767f2fe7d6c
Neilsen Morris <Nmrubix11@gmail.com>
2025-07-02 21:13:55 +0000

    I got the folders straitened out

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css'
+
+
+
+
+
+export const App = () => {
+ 
+
+  return (
+  <div className="home container-fluid"
+      style={{
+        backgroundImage: `url(${hairBG})`,
+        backgroundSize: "contain",
+        backgroundPosition: "center", 
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+      }}>
+
+  </div>
+  )
+}
+
+export default App
