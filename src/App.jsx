3e461c603fddc7bc981a0dd3bf241eccb71316c3
Neilsen Morris <Nmrubix11@gmail.com>
2025-07-03 21:22:23 +0000

    start populating pages and get navbar to render in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 import hairBlesserOfficial from "./assets/hairBlesserOfficial.png";
 
 
-
 export const App = () => {
- 
-
   return (
-  <div className="home container-fluid"
+    <div
+      className="home container-fluid d-flex justify-content-center align-items-center"
       style={{
         backgroundImage: `url(${hairBlesserOfficial})`,
         backgroundSize: "cover",
-        backgroundPosition: "center", 
+        backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         height: "100vh",
         width: "100vw",
-      }}>
-        <div>
-          
-        </div>
+      }}
+    >
+      <div className="book-nowBtn">
+        <button type="button" className="custom-btn btn-lg">
+          Book Now
+        </button>
+      </div>
+    </div>
+  );
+};
 
-  </div>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-transparent px-4 position-fixed w-100"
+      style={{ zIndex: 1000 }}
+    >
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          HairBlesser
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-navbar"
+          aria-controls="main-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="main-navbar">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/services"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Book
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
